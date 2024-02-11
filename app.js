f3b54dd0ade739e6508a3cd737b0cb2d4048d388
Jon <jonathon_rylatt@yahoo.com>
2024-02-11 17:18:49 +0800

    Finished all 5 exercises

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
-
+const firstName = "Jon";
+lastName = "Rylatt";
+age = 29;
 // Exercise 3
+let langauge = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookpark = 456;
+let hasRead = true;
